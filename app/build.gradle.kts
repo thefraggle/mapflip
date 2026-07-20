3998ee49720bac5b2e7d081d159268583fc82241
Daniel Notthoff <daniel.notthoff@gmail.com>
2026-07-20 16:02:46 +0200

    fix: resolve keystore path relative to root project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     signingConfigs {
         create("release") {
             if (keystoreProperties.isNotEmpty()) {
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
