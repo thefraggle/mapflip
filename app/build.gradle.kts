2e0a13d3bb857d0130fcd043c7efe745777d9f27
Daniel Notthoff <daniel.notthoff@gmail.com>
2026-09-17 21:55:33 +0200

    fix(build): declare literal versionCode and versionName for fdroid parser compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "de.goork.mapflip"
         minSdk = 26
         targetSdk = 36
-        versionCode = appVersionCode
-        versionName = appVersion
+        versionCode = 315026
+        versionName = "1.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
